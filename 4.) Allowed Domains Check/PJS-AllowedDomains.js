9ef76de74c9486ec6df2e284eed383d0e6a22de4
Ed DeGagne <edward.degagne@optimizely.com>
2024-04-21 15:47:35 -0400

    Added Allowed Domain Check

---
+++ 4.) Allowed Domains Check/PJS-AllowedDomains.js
@@ -0,0 +1,22 @@
+window.optimizely = window.optimizely || [];    // Omit if already instantiating in current project js
+
+// PJS: Check list of allowed domains - modify this list for your domain(s)
+var allowedOptimizelyDomains = ['firstdomain.com', 'seconddomain.com', 'sub.firstdomain.com'];
+
+// Check allowed Optimizely Web domains
+function checkOptimizelyDomains() {
+    var currentDomain = window.location.hostname;
+
+    // If not in the allowed list, disable Optimizely
+    if (!allowedOptimizelyDomains.includes(currentDomain)) {
+        window.optimizely.push({
+            "type": "disable"
+        });
+        console.log(`PJS: ${currentDomain} is not an allowed domain. Optimizely Web is disabled.`);
+    } else {
+        console.log(`PJS: ${currentDomain} is an allowed domain. Optimizely Web is enabled.`);
+    }
+}
+
+// Call function to check if requested domain is allowed
+checkOptimizelyDomains();
--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Javascript code to track scroll depth and send as an Optimizely custom event for
 
 ## 3.) PJS-Session Count Tracking
 Javascript code to track visitor session count and send as an Optimizely custom event.
+
+## 4.) PJS-Allowed Domain Check
+Javascript code to check allowed domains via a configurable array list. Domains not listed in the array will result in the snippet being disabled.
