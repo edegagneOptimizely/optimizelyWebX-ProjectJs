308d851af4d256d526b27cba4370541dce67bb8b
Ed DeGagne <edward.degagne@optimizely.com>
2024-04-21 14:57:07 -0400

    Added documention and two new files

--- PJS-One Trust Consent.js
+++ PJS-One Trust Consent.js
@@ -1,10 +1,12 @@
+window.optimizely = window.optimizely || [];    // Omit if already instantiating in current project js
+
 // Check OneTrust for consent to run Optimizely Web X
 function optiOneTrustCheck() {
 	const allowedGroup = 'C0003';		// Change this to the category used for Optimizely consent
 	const cookieName = 'OptanonConsent';
 	const cookies = document.cookie.split(';');
 	let cookieValue = '';
-	
+
 	// Get cookie value
 	for (let i = 0; i < cookies.length; i++) {
 		const cookie = cookies[i].trim();
@@ -17,7 +19,7 @@ function optiOneTrustCheck() {
 	const decodedcookieValue = decodeURIComponent(cookieValue);
 	const keyValuePairs = decodedcookieValue.split('&');
 	let consentGroups = null;
-	
+
 	// Extract consent groups from cookie value
 	for (let i = 0; i < keyValuePairs.length; i++) {
 		const pair = keyValuePairs[i].split('=');
@@ -32,16 +34,14 @@ function optiOneTrustCheck() {
 
 	// Check for consent
 	if (!consentGroups.indexOf(allowedGroup + ':1')) {
-		//console.log('Optimizely disabled by consent');
-		
-		window.optimizely = window.optimizely || [];
-		window["optimizely"].push({
+		console.log('PJS: Optimizely disabled by consent');
+		window.optimizely.push({
 			"type": "disable"
-		});		
+		});
 	} else {
-		//console.log('Optimizely enabled by consent');
+		console.log('PJS: Optimizely enabled by consent');
 	}
 }
 
-// Usage
+// Call this function to check consent when project/snippet loads
 optiOneTrustCheck();
--- README.md
+++ README.md
@@ -12,3 +12,19 @@ By default, the group/category structure is 'C0001:1, C0002:1, C0003:1, C0004:1'
 - Targeting Cookies (C0004)
 
 To use, make sure the 'allowedGroup' value in the function matches the category you place Optimizely Web X in. By default, this is set to group C0003.
+
+## PJS-Scrolling Metrics
+These javascript functions will check scroll depth and raise an Optimizely custom event for specified scoll depths.
+
+Place the javascript in project js. Then create the following four (4) custom events:
+- Scroll 25 (scroll25)
+- Scroll 50 (scroll50)
+- Scroll 50 (scroll75)
+- Scroll 50 (scroll100)
+
+These events will now be availably globally to use as instrumentation metrics in your campaigns/experiments.
+
+## PJS-Session Count Tracking
+This javascript function will track session count as an Optimizely custom event.
+
+This event will now be availably globally to use as an instrumentation metric in your campaigns/experiments.
