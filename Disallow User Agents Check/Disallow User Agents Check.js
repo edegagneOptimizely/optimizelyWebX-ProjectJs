bdf8b65d827ecb19d5315c5d4adeeb3071ac6637
Ed DeGagne <edward.degagne@optimizely.com>
2026-02-20 09:50:27 -0500

    Add 'DatadogSynthetics' to disallowed user agents
    Renamed a couple of variables for clarity

--- Disallow User Agents Check/Disallow User Agents Check.js
+++ Disallow User Agents Check/Disallow User Agents Check.js
@@ -1,21 +1,22 @@
 window.optimizely = window.optimizely || []; // Omit if already instantiating in current project js
 
 (() => {
-  // PJS: Configure your disallowed user agents here
-  const optlyUserAgentList = ["Prerender", "PetalBot", "Checkly"];
+  // PJS: Configure your disallowed user agents
+  const optiDisallowedAgents = ["Prerender", "PetalBot", "Checkly", "DatadogSynthetics"];
 
-  if (isUserAgentBot()) {   
+	// PJS: Disable Opti Web X if request agent is disallowed
+  if (isDisallowedAgent()) {   
     window.optimizely.push({
       "type": "disable"
     });
   }
 
-  function isUserAgentBot() {
+  function isDisallowedAgent() {
     
     var result = false;
     var lowerUserAgent = navigator.userAgent.toLowerCase();  
-    for (var i = 0; i < optlyUserAgentList.length; i++) {
-      if (lowerUserAgent.includes(optlyUserAgentList[i].toLowerCase())) {
+    for (var i = 0; i < optiDisallowedAgents.length; i++) {
+      if (lowerUserAgent.includes(optiDisallowedAgents[i].toLowerCase())) {
         // User agent is in the list
         console.log(`PJS: ${navigator.userAgent} is not an allowed user agent. Optimizely Web will be disabled.`);
         result = true;
